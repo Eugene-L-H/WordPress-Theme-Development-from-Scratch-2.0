f0cf71071da759f173b34251d558df0677e47ed8
Eugene <eugenehelland@mail.com>
2023-09-13 21:01:50 -0600

    Build out basic html structure for index.php.

---
+++ wp-devs/index.php
@@ -0,0 +1,59 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php body_class(); ?>>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat earum, ullam quae nostrum suscipit facilis quidem
+    iure incidunt odit. Reprehenderit ipsam provident molestias amet, recusandae itaque voluptatem dolores qui labore?
+  </p>
+  <div id="page" class="site">
+    <header>
+      <!-- -->
+      <section class="top-bar">
+        <div class="logo">
+          Logo
+        </div>
+        <div class="searchbox">
+          Search
+        </div>
+      </section>
+      <section class="menu-area">
+        <nav class="main-menu">
+          Menu
+        </nav>
+      </section>
+    </header>
+    <div id="content" class="site-content">
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+          <section class="hero">
+            Hero
+          </section>
+          <section class="services">
+            Services
+          </section>
+          <section class="home-blog">
+            Blog
+          </section>
+        </main>
+      </div>
+
+      <!--footer-->
+      <footer class="site-footer">
+        Footer
+      </footer>
+    </div>
+  </div>
+</body>
+
+</html>
