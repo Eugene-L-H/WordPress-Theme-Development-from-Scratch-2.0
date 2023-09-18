dbd9e92dc6156d4a5909000168de0ef1a44b0d45
Eugene <eugenehelland@mail.com>
2023-09-17 18:39:03 -0600

    Add initial post fetching to index.php.

--- wp-devs/index.php
+++ wp-devs/index.php
@@ -10,7 +10,39 @@
         Services
       </section>
       <section class="home-blog">
-        Blog
+        <div class="container">
+          <?php
+          if (have_posts()):
+            while (have_posts()):
+              the_post();
+              ?>
+              <article>
+                <h2>
+                  <?php the_title(); ?>
+                </h2>
+                <div class="meta-info">
+                  <p>Posted in
+                    <?php echo get_the_date(); ?> by
+                    <?php the_author_posts_link(); ?>
+                  </p>
+                  <p>Categories:
+                    <?php the_category(', '); ?>
+                  </p>
+                  <p>
+                    <?php the_tags('', ','); ?>
+                  </p>
+                </div>
+                <?php the_content(); ?>
+              </article>
+                  <?php
+            endwhile;
+          else:
+            ?>
+              <p>There are no posts!</p>
+          <?php endif;
+          ?>
+        </div>
+
       </section>
     </main>
   </div>
