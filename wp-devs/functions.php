57c41071269c8ca32881e457412d5330b5b0a19b
Eugene <eugenehelland@mail.com>
2023-09-18 16:38:06 -0600

    Add support for custom thumbnails in admin dashboard. Custom thumbnails are loaded on the blog page.

--- wp-devs/functions.php
+++ wp-devs/functions.php
@@ -28,6 +28,9 @@ function wpdevs_config()
     'width' => 1920
   );
   add_theme_support('custom-header', $args);
+
+  // Enable custom thumbnails for posts 
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'wpdevs_config', 0);
