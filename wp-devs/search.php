353cefb13929331ed2a74b3551295fa2b9e662f4
Eugene <eugenehelland@mail.com>
2023-09-20 15:50:09 -0600

    Add "if" statement to search.php where meta-data are ommitted from search results where "post" is not the post-type (such as pages).

--- wp-devs/search.php
+++ wp-devs/search.php
@@ -10,28 +10,34 @@
         the_post();
         ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
           <header>
             <h2>
               <a href="<?php the_permalink(); ?>">
                 <?php the_title(); ?>
               </a>
             </h2>
-            <!-- Add in featured image -->
-            <div class="featured-image">
-              <?php the_post_thumbnail([275, 275]); ?>
-            </div>
-            <div class="meta-info">
-              <p>Posted:
-                <?php echo get_the_date(); ?> by
-                <?php the_author_posts_link(); ?>
-              </p>
-              <p>Categories:
-                <?php the_category(', '); ?>
-              </p>
-              <p>
-                <?php the_tags('', ','); ?>
-              </p>
-            </div>
+
+            <!-- if statement that will display the meta info if "post" type equals "post" -->
+            <?php if (get_post_type() === 'post'): ?>
+              <div class="meta-info">
+                <!-- Add in featured image -->
+                <div class="featured-image">
+                  <?php the_post_thumbnail([275, 275]); ?>
+                </div>
+                <p>Posted:
+                  <?php echo get_the_date(); ?> by
+                  <?php the_author_posts_link(); ?>
+                </p>
+                <p>Categories:
+                  <?php the_category(', '); ?>
+                </p>
+                <p>
+                  <?php the_tags('', ','); ?>
+                </p>
+              </div>
+            <?php endif; ?>
+
           </header>
 
           <?php the_excerpt(); ?>
